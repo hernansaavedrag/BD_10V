83d257b17c673797b9bcc1195069d848673fdf0a
Hernan Saavedra <hernan.saavedra@gmail.com>
2021-11-17 23:25:57 -0300

    Subo agregar libro

--- src/controlador/Registro.java
+++ src/controlador/Registro.java
@@ -1,5 +1,12 @@
 package controlador;
 
+import bd.Conexion;
+import java.util.Date;
+import modelo.Libro;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author Profe Hernán
@@ -7,6 +14,35 @@ package controlador;
  */
 public class Registro {
     
-    
+    public boolean agregar(Libro libro){
+        
+        Date date;
+        try {
+            
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+            
+            date = libro.getPublicacion();
+            
+            String query = "INSERT INTO libro(titulo,autor,publicacion,precio,disponible) VALUES (?,?,?,?,?)";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setString(1, libro.getTitulo());
+            stmt.setString(2, libro.getAutor());
+            stmt.setDate(3,new java.sql.Date(date.getTime()));
+            stmt.setInt(4, libro.getPrecio());
+            stmt.setBoolean(5, libro.isDisponible());
+            
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error SQL al agregar Libro");
+            return false;
+        } catch(Exception e){
+            System.out.println("Error al agregar libro");
+            return false;
+        }
+    }
     
 }

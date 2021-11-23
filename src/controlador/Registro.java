294ce806510fef623cb82dd84389a86bc20dd253
Hernan Saavedra <hernan.saavedra@gmail.com>
2021-11-22 22:34:00 -0300

    subiendo CUD (falta R)

--- src/controlador/Registro.java
+++ src/controlador/Registro.java
@@ -45,4 +45,64 @@ public class Registro {
         }
     }
     
+    public boolean actualizar(Libro libro){
+        
+        Date date;
+        try {
+            
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+            
+            date = libro.getPublicacion();
+            
+            String query = "UPDATE libro SET titulo=?,autor =?,publicacion=?,precio=?,disponible=? WHERE = idlibro=?";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setString(1, libro.getTitulo());
+            stmt.setString(2, libro.getAutor());
+            stmt.setDate(3,new java.sql.Date(date.getTime()));
+            stmt.setInt(4, libro.getPrecio());
+            stmt.setBoolean(5, libro.isDisponible());
+            stmt.setInt(6, libro.getIdLibro());
+            
+            
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error SQL al Actualizar Libro");
+            return false;
+        } catch(Exception e){
+            System.out.println("Error al Actualizar libro");
+            return false;
+        }
+    }
+    
+    public boolean eliminar(int idLibro){
+        
+       
+        try {
+            
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+            
+            
+            String query = "DELETE FROM libro WHERE idlibro=?";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setInt(1,idLibro);
+ 
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error SQL al eliminar Libro");
+            return false;
+        } catch(Exception e){
+            System.out.println("Error al eliminar libro");
+            return false;
+        }
+        
+    }
+    
 }
